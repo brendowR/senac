9de21a6d0bbdf483c41db1eda23154d829b72ea3
Brendow Ribeiro <51176022+brendowR@users.noreply.github.com>
2019-06-19 15:44:21 -0300

    Create modelo.dart

---
+++ Aulas_Thiago/Aula4/modelo.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+//Método Principal
+void main(){
+  runApp(MyApp());
+}
+
+//Classe MyApp
+class MyApp extends StatefulWidget{
+    @override
+  State<StatefulWidget> createState(){
+      return _MyAppState();
+    }
+}
+
+//Classe _MyAppState que herda(extends) a classe State
+class _MyAppState extends State<MyApp> {
+
+  //Interface #################################
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Exemplo de Classes e Objetos",
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text("Classes e Objetos"),
+            ),
+          ),
+        )
+    );
+  }
+}
